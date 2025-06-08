a50470e49cf1f13bd29062dddd5dc38539ea9e77
Ameer Bajwa <ameer.bajwa@revelsystems.com>
2025-06-08 12:43:22 -0700

    Removing storyboard from project so UI can be written programmatically

--- PokemonLeafGreenApp-UIKit/ViewController.swift
+++ PokemonLeafGreenApp-UIKit/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var titleLabel: UILabel!
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.view.backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        titleLabel = UILabel()
+        titleLabel.text = "POKEMON LEAFGREEN APP"
+        titleLabel.font = .systemFont(ofSize: 20.0)
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+        
+        self.view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20.0),
+            titleLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20.0)
+        ])
     }
 
 
