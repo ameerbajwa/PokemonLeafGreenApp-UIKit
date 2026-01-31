984aca2320ee6cc80db21161bdd9d5f8478b11f4
AmeerBajwaRevelup <ameer.bajwa@revelsystems.com>
2026-01-31 17:38:00 -0500

    Created classes PokemonLocationConfiguratioNode and PokemonLocationConfigurationDoublyLinkedList

---
+++ PokemonLeafGreenApp-UIKit/Utilities/Configurations/PokemonLocationConfigurationDoublyLinkedList.swift
@@ -0,0 +1,48 @@
+//
+//  PokemonLocationConfigurationDoublyLinkedList.swift
+//  PokemonLeafGreenApp-UIKit
+//
+//  Created by Ameer Bajwa on 1/31/26.
+//
+
+class PokemonLocationConfigurationNode {
+    var configuration: PokemonLocationConfiguration
+    var previousConfiguration: PokemonLocationConfigurationNode?
+    var nextConfiguration: PokemonLocationConfigurationNode?
+    
+    init(configuration: PokemonLocationConfiguration) {
+        self.configuration = configuration
+    }
+}
+
+public class PokemonLocationConfigurationDoublyLinkedList {
+    var startLocation: PokemonLocationConfigurationNode?
+    var lastLocation: PokemonLocationConfigurationNode?
+    
+    func appendLocation(configuration: PokemonLocationConfiguration) {
+        let newPokemonLocationConfigurationNode = PokemonLocationConfigurationNode(configuration: configuration)
+        
+        guard lastLocation != nil else {
+            startLocation = newPokemonLocationConfigurationNode
+            lastLocation = newPokemonLocationConfigurationNode
+            return
+        }
+        
+        lastLocation?.nextConfiguration = newPokemonLocationConfigurationNode
+        newPokemonLocationConfigurationNode.previousConfiguration = lastLocation
+        self.lastLocation = newPokemonLocationConfigurationNode
+    }
+    
+    func findLocation(locationName: String) -> PokemonLocationConfigurationNode? {
+        var currentLocationNode = startLocation
+        
+        while currentLocationNode != nil {
+            if currentLocationNode?.configuration.name == locationName {
+                return currentLocationNode
+            }
+            currentLocationNode = currentLocationNode?.nextConfiguration
+        }
+        
+        return nil
+    }
+}
